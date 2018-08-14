6ef3e6b831a711e100106436545da8ca46eda2f9
ultimatile <kaitaiti@live.jp>
2018-08-15 05:01:37 +0900

    3rd commit

--- MC.jl
+++ MC.jl
@@ -1,7 +1,9 @@
-L=6
+#classical J1-J2 Heisenberg model
+#Metropolis method
+#Annealing
+L=8
 Tmin=0.1;Tmax=2;Tsteps=20
 deltaT=(Tmax-Tmin)/Tsteps
-#T=0.1
 J1=1;J2=0
 sintheta(c)=sqrt(abs(1-c^2))
 SiSj(pi,pj,ti,tj)=cos(pi-pj)*sintheta(ti)*sintheta(tj)+ti*tj
@@ -9,22 +11,17 @@ mcs_max=10000
 runs=5
 discard=mcs_max/2
 frac=mcs_max-discard
-ave_spec=zeros(Tsteps)
-var_spec=zeros(Tsteps)
+ave_spec=zeros(Tsteps);var_spec=zeros(Tsteps)
 phi=zeros(L,L);costheta=zeros(L,L)
 iphi=zeros(L,L);icostheta=zeros(L,L)
 ip=zeros(Int,L);im=zeros(Int,L)
-for i in 1:L
-    ip[i]=i+1
-    im[i]=i-1
-end
+for i in 1:L;ip[i]=i+1;im[i]=i-1;end
 ip[L]=1;im[1]=L
 for run in 1:runs
 phi=2*pi*rand!(iphi)
 costheta=rand!(icostheta)
     for Tstep in 1:Tsteps
         energy=0;energy2=0
-        #auto=0
         T=Tmax-deltaT*(Tstep-1)
         for mcs in 1:mcs_max
             tmpH=0
@@ -57,34 +54,21 @@ costheta=rand!(icostheta)
                     prob=rand()
                     if exp(-deltaE/T)>prob
                         phi[ix,iy]=newphi;costheta[ix,iy]=newcostheta
-                        if mcs>discard
-                            tmpH+=newE
-                            #auto+=SiSj(phi[ix,iy],iphi[ix,iy],costheta[ix,iy],icostheta[ix,iy])
-                        end
+                        if mcs>discard;tmpH+=newE;end
                     else
-                        if mcs>discard
-                            tmpH+=tmpE
-                            #auto+=SiSj(phi[ix,iy],iphi[ix,iy],costheta[ix,iy],icostheta[ix,iy])
-                        end
+                        if mcs>discard;tmpH+=tmpE;end
                     end
-                    #println("$deltaE $(exp(-deltaE/T)) $prob")
                 end
             end
-            energy+=tmpH
-            energy2+=tmpH^2
-            #println(auto/L/L/mcs)
+            energy+=tmpH;energy2+=tmpH^2
         end
-        energy/=frac*2
-        energy2/=frac*4
-        #println("$T $energy $energy2" )
+        energy/=frac*2;energy2/=frac*4
         spec=(energy2-energy^2)/T^2/L^2
         ave_spec[Tstep]+=spec
         var_spec[Tstep]+=spec^2
        end
 end
-ave_spec/=runs
-var_spec/=runs
-
+ave_spec/=runs;var_spec/=runs
 for Tstep in 1:Tsteps
     println("$(Tmax-deltaT*(Tstep-1)) $(ave_spec[Tstep]) $(sqrt((var_spec[Tstep]-ave_spec[Tstep]^2)/(runs-1)))")
 end
