ff5464d1e2671107bea95e9600109f87481aea81
ultimatile <kaitaiti@live.jp>
2018-09-23 13:47:56 +0900

    add plot script

---
+++ C_MPvsHB.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn
+import numpy as np
+data = np.genfromtxt("C_HB.dat")
+x=data[:,0]
+y=data[:,1]
+yerr=data[:,2]
+plt.errorbar(x,y,yerr=yerr,fmt='x')
+plt.scatter(x,y,marker='x',label="heatbath")
+
+data = np.genfromtxt("C_MP.dat")
+x=data[:,0]
+y=data[:,1]
+yerr=data[:,2]
+plt.errorbar(x,y,yerr=yerr,fmt='x')
+plt.scatter(x,y,marker='x',label="Metropolis")
+plt.xlim(0,)
+plt.xlabel("T")
+plt.ylabel("C")
+
+plt.legend()
+plt.show()
